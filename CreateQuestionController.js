c0ec8ec476cc01c508f273bfdb2e42d075d57f6e
Jorge Gallardo <jorge8486@gmail.com>
2015-08-29 15:24:09 -0400

    switched question submit from jquery to angular

---
+++ CreateQuestionController.js
@@ -0,0 +1,22 @@
+var app = angular.module('questionsApp', []);
+
+app.controller('CreateQuestionController', function ($scope) {
+  Parse.initialize("2Hok8QvZcIKAfvyNu28JPTzjhhJwimSC9t26xq6w", "v6GrWsGgtqU8maRaSK1JUQ0mkvS7FKdn8ohQkFVf");
+  var Question = Parse.Object.extend("Question");
+
+  $scope.submitQuestion = function() {
+    var newQuestion = new Question();
+    newQuestion.set("questionText", $scope.question.text);
+    newQuestion.set("answerChoices", [$scope.question.answerChoice1, $scope.question.answerChoice2, $scope.question.answerChoice3, $scope.question.answerChoice4]);
+    newQuestion.save(null, {
+      success: function(newQuestion) {
+        console.log("New object created with objectId: " + newQuestion.id);
+      },
+      error: function(error) {
+        console.log("Error: " + error.message);
+      }
+    });
+
+    $scope.question = {};
+  }
+});
